e534291f8191b0bdbcd485f6eb95a297a2f0d04a
Idiotic Enigma <idiotic-enigma@outlook.com>
2021-08-25 18:04:01 +0100

    Initial solution to challenge

---
+++ 05-flex-panels/script.js
@@ -0,0 +1,10 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() { this.classList.toggle('open'); }
+
+function toggleActive(e) {
+	e.propertyName.includes('flex') ? this.classList.toggle('open-active') : null;
+}
+
+panels.forEach(	panel => panel.addEventListener('click', toggleOpen) );
+panels.forEach(	panel => panel.addEventListener('transitionend', toggleActive) );
