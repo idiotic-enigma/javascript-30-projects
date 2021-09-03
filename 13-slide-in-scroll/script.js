29996d1c87cdd748112e277409de15745bab3fa7
Idiotic Enigma <idiotic-enigma@outlook.com>
2021-09-03 15:22:48 +0100

    Initial solution to challenge

--- 13-slide-in-scroll/script.js
+++ 13-slide-in-scroll/script.js
@@ -13,3 +13,23 @@ function debounce(func, wait = 20, immediate = true) {
 		if (callNow) func.apply(context, args);
 	};
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(event) {
+	sliderImages.forEach(image => {
+		/*	Equal to the vertical scroll height, plus the viewport height,
+				then minus half the image height */
+		const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+		const imageBottom = image.offsetTop + image.height;
+
+		/*	Conditions are:
+				- Image is at least half-visible on the page
+				- Image has not been completely scrolled past
+		*/
+		if(slideInAt > image.offsetTop && window.scrollY < imageBottom) image.classList.add('active');
+		else image.classList.remove('active');
+	});
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
