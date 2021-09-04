879303bac853c63ef26e0844566b6703e15c22d6
Idiotic Enigma <idiotic-enigma@outlook.com>
2021-09-04 14:40:27 +0100

    Initial solution to challenge

--- 17-band-name-sort/script.js
+++ 17-band-name-sort/script.js
@@ -1,15 +1,18 @@
-const bands = [
-	'The Plot in You',
-	'The Devil Wears Prada',
-	'Pierce the Veil',
-	'Norma Jean',
-	'The Bled',
-	'Say Anything',
-	'The Midway State',
-	'We Came as Romans',
-	'Counterparts',
-	'Oh, Sleeper',
-	'A Skylit Drive',
-	'Anywhere But Here',
-	'An Old Dog'
-];
+function stripArticles(name) { return name.replace( /^(a |an |the )/i, '' ).trim(); }
+
+function addToList(band = '') {
+	if(!band) return;
+
+	const listItem = document.createElement('li');
+	listItem.textContent = band;
+	bandList.appendChild(listItem);
+}
+
+const bands = ['The Plot in You','The Devil Wears Prada','Pierce the Veil','Norma Jean','The Bled','Say Anything','The Midway State','We Came as Romans','Counterparts','Oh, Sleeper','A Skylit Drive','Anywhere But Here','An Old Dog'];
+const bandsSorted = bands.sort( (a, b) => stripArticles(a) > stripArticles(b) ? 1 : -1 );
+
+const bandList = document.querySelector('#bands');
+
+bandsSorted.forEach(band => addToList(band));
+
+console.table(bandsSorted);
