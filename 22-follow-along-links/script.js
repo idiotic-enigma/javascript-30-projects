3bb8f57c2da6c0bf33eb9546b3e21e218047f39e
Idiotic Enigma <idiotic-enigma@outlook.com>
2021-09-16 18:09:37 +0100

    Initial solution to challenge

--- 22-follow-along-links/script.js
+++ 22-follow-along-links/script.js
@@ -1,1 +1,21 @@
-// 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function moveHighlight() {
+	const clientRect = this.getBoundingClientRect();
+	const attributes = {
+		x: clientRect.left + window.scrollX,
+		y: clientRect.top + window.scrollY,
+		width: clientRect.width,
+		height: clientRect.height
+	};
+
+	highlight.style.width = `${attributes.width}px`;
+	highlight.style.height = `${attributes.height}px`;
+	highlight.style.transform = `translate(${attributes.x}px, ${attributes.y}px)`;
+}
+
+triggers.forEach( link => link.addEventListener('mouseenter', moveHighlight) );
