52409095f8d7355d9e0ce3924f81b98e0d5776cf
Idiotic Enigma <idiotic-enigma@outlook.com>
2021-09-16 19:16:54 +0100

    Initial solution to challenge

--- 23-speech-synthesis/script.js
+++ 23-speech-synthesis/script.js
@@ -4,3 +4,41 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+function populateVoices() {
+	voices = this.getVoices();
+	const voiceOptions = voices
+		.filter( voice => voice.lang.includes('en') )
+		.map(voice => {
+			const option = document.createElement('option');
+			option.setAttribute('value', voice.name);
+			option.textContent = `${voice.name} (${voice.lang})`;
+			return option;
+		})
+	;
+
+	voiceOptions.forEach(option => { voicesDropdown.appendChild(option); });
+}
+
+function changeVoice() {
+	msg.voice = voices.find(voice => voice.name === this.value);
+	toggleState();
+}
+
+function toggleState(restart = true) {
+	speechSynthesis.cancel();
+	if(restart) { speechSynthesis.speak(msg) };
+}
+
+function setOption() {
+	msg[this.name] = this.value;
+	toggleState();
+}
+
+msg.text = document.querySelector('[name="text"]').value;
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', changeVoice);
+options.forEach( option => option.addEventListener('change', setOption) );
+speakButton.addEventListener('click', toggleState);
+stopButton.addEventListener('click', toggleState.bind(null, false));
